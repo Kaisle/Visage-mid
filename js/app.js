49c8977259b3b733df184e8e88b1b72e5e19d6a6
Anders Borum <asoggiorno@gmail.com>
2015-07-26 14:40:21 +0200

    Restyled replay section

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 var app = angular.module("mainApp", []);
-app.controller("mainCtrl", function ($scope) {
+app.controller("mainCtrl", ['$scope', '$window', function ($scope, $window) {
     $scope.message = "Hello World";
     $scope.heroesURL = "http://cdn.dota2.com/apps/dota2/images/heroes/";
     $scope.sb = "_sb.png"; 
     $scope.itemsURL = "http://cdn.dota2.com/apps/dota2/images/items/";
-    $scope.lg = "_lg.png"; 
-});
+    $scope.lg = "_lg.png";
+    $scope.replayUrl = "https://www.youtube.com/playlist?list=PL35b-n5k1iH-ayVVtOz30W815QiVLvvlH";
+    $scope.goToReplays = function () {
+        $window.location.href = $scope.replayUrl;
+    }
+}]);
